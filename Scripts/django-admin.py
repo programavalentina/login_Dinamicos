0e5b6812f87328ac914855f4e44035723fcff425
Santiago Xoyón <xoyonsantiago@gmail.com>
2018-11-30 13:35:03 -0600

    Proyecto

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sku 30312\documents\proyectospython\loginconregistro\entorno\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
